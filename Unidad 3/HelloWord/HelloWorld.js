08788c30db2f71b4d79ddb947241ab015f54962b
Miguel Cabrera <corderom776@gmail.com>
2024-11-08 07:55:58 -0600

    act

--- Unidad 3/HelloWord/HelloWorld.js
+++ Unidad 3/HelloWord/HelloWorld.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const app = express();
+
+app.use(express.json());
+app.use(express.text());
  
+app.get('/administrativos',(req,res)=>{
+  console.log(req.query)
+   res.send('Servidores contestando a peticiones GET')
+  })
+
+app.get('/maestros',(req,res)=>{
+      console.log(req.body)
+       res.send('Servidores contestando a peticiones GET')
+      })
+
+
+app.get('/Estudiantes/carrera',(req,res)=>{
+console.log(req.params.carrera)
+console.log(req.query.control)
+res.send('Servidores contestando a peticiones GET')
+})
+
 app.get('/', (req, res) => {
   res.send('Hola Mundo');  // Envía "Hola Mundo" como respuesta
 });
@@ -8,3 +28,4 @@ app.get('/', (req, res) => {
 app.listen(8082, () => {
   console.log('Servidor Express escuchando en el puerto 8082');
 });
+
